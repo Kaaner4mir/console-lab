505102c2607530bbb47ec06c2685d270a14aa0fd
Kaaner <kaaner4mir@gmail.com>
2025-09-15 17:38:06 +0300

    Add perimeter case

--- GeoCalculator/Startup.cs
+++ GeoCalculator/Startup.cs
@@ -1,7 +1,32 @@
-﻿class Startup
+﻿using System.Text;
+
+class Startup
 {
     public static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
 
+        try
+        {
+            while (true)
+            {
+                Console.Clear();
+                Menu.DisplayMainMenu();
+                short choice = ConsoleHelper.GetInput<short>("\n👉 Select the action you want to perform : ");
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1: Perimeter.PerimeterOperation(); break;
+                    default: ConsoleHelper.WriteColored("❓ The operation you attempted failed.", ConsoleColor.Yellow); break;
+                }
+                ConsoleHelper.WaitingScreen();
+                ConsoleHelper.SpinnerAnimation();
+            }
+        }
+        catch (Exception exc)
+        {
+            ConsoleHelper.WriteColored($"\n⚠️ An error has occurred : {exc.Message}", ConsoleColor.Red);
+        }
     }
 }
